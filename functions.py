9b9e4bb4712b3b8a0a09b48ab781a52644ca9358
steben_op <kostya.battlefield13@mail.ru>
2023-04-04 21:18:40 +0400

    [upd] --add second and half of third exercise

--- functions.py
+++ functions.py
@@ -1,7 +1,80 @@
 from keras import models, layers
+import matplotlib.pyplot as plt
 
 
-def lay(mode, x_train, y_train, x_test, y_test):
+def graph_2(history):
+    loss = history.history['loss']
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, acc, 'bo', label='Training loss')
+    plt.plot(epochs, val_acc, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+def graph_1(history):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+def batch_2(batch, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val):
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(46, activation='softmax'))
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+    model.fit(partial_x_train, partial_y_train, epochs=4, batch_size=batch, validation_data=(x_val, y_val), verbose=0)
+    results = model.evaluate(x_test, one_hot_test_labels, verbose=0)
+    print(f'Выборка - {batch} ', 'Потери -', results[0], 'Точность -', results[1])
+
+
+def lay_2(layers_count, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val):
+    model = models.Sequential()
+    if layers_count == 1:
+        model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+        model.add(layers.Dense(46, activation='softmax'))
+    elif layers_count == 2:
+        model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(46, activation='softmax'))
+    else:
+        model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(46, activation='softmax'))
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+    model.fit(partial_x_train, partial_y_train, epochs=4, batch_size=512, validation_data=(x_val, y_val), verbose=0)
+    results = model.evaluate(x_test, one_hot_test_labels, verbose=0)
+    print(f'Количество слоёв - {layers_count} ', 'Потери -', results[0], 'Точность -', results[1])
+
+
+def epochs_count(epo, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val):
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(46, activation='softmax'))
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+    model.fit(partial_x_train, partial_y_train, epochs=epo, batch_size=512, validation_data=(x_val, y_val), verbose=0)
+    results = model.evaluate(x_test, one_hot_test_labels, verbose=0)
+    print(f'Количество эпох - {epo}', 'Потери -', results[0], 'Точность -', results[1])
+
+
+def lay_1(mode, x_train, y_train, x_test, y_test):
     model = models.Sequential()
     if mode == 1:
         model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
@@ -50,7 +123,7 @@ def relu_tanh(mode, x_train, y_train, x_test, y_test):
     print(f'Функция активации {mode}', 'Потери -', results[0], 'Точность -', results[1])
 
 
-def batch(batch_size, x_train, y_train, x_test, y_test):
+def batch_1(batch_size, x_train, y_train, x_test, y_test):
     model = models.Sequential()
     model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
     model.add(layers.Dense(16, activation='relu'))
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from tensorflow import keras
 from keras.utils import to_categorical
-from keras.datasets import imdb, mnist, reuters
+from keras.datasets import imdb, mnist, reuters, boston_housing
 from keras import layers, models
 import numpy as np
 
-from functions import loses, relu_tanh, batch, neuron, lay
+from functions import loses, relu_tanh, batch_1, neuron, lay_1, epochs_count, lay_2, batch_2, graph_1, graph_2
 
 
 def first_exercise():
@@ -43,9 +43,9 @@ def first_exercise():
     y_test = np.asarray(test_labels).astype('float32')
 
     print('___________________________')
-    lay(1, x_train, y_train, x_test, y_test)
-    lay(2, x_train, y_train, x_test, y_test)
-    lay(3, x_train, y_train, x_test, y_test)
+    lay_1(1, x_train, y_train, x_test, y_test)
+    lay_1(2, x_train, y_train, x_test, y_test)
+    lay_1(3, x_train, y_train, x_test, y_test)
     print('___________________________')
     neuron(32, 32, x_train, y_train, x_test, y_test)
     neuron(32, 64, x_train, y_train, x_test, y_test)
@@ -59,12 +59,12 @@ def first_exercise():
     relu_tanh('relu', x_train, y_train, x_test, y_test)
     relu_tanh('tanh', x_train, y_train, x_test, y_test)
     print('___________________________')
-    batch(32, x_train, y_train, x_test, y_test)
-    batch(64, x_train, y_train, x_test, y_test)
-    batch(96, x_train, y_train, x_test, y_test)
-    batch(128, x_train, y_train, x_test, y_test)
-    batch(256, x_train, y_train, x_test, y_test)
-    batch(512, x_train, y_train, x_test, y_test)
+    batch_1(32, x_train, y_train, x_test, y_test)
+    batch_1(64, x_train, y_train, x_test, y_test)
+    batch_1(96, x_train, y_train, x_test, y_test)
+    batch_1(128, x_train, y_train, x_test, y_test)
+    batch_1(256, x_train, y_train, x_test, y_test)
+    batch_1(512, x_train, y_train, x_test, y_test)
     print('////////////////////////////////////////////////////')
 
 
@@ -88,7 +88,7 @@ def second_exercise():
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(46, activation='softmax'))
 
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
 
     x_val = x_train[:1000]
     partial_x_train = x_train[1000:]
@@ -96,26 +96,80 @@ def second_exercise():
     y_val = one_hot_train_labels[:1000]
     partial_y_train = one_hot_train_labels[1000:]
 
-    model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val), verbose=0)
+    history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val),
+                        verbose=0)
 
-    model = models.Sequential()
-    model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(46, activation='softmax'))
+    graph_1(history)
+    graph_2(history)
 
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val), verbose=0)
-    result = model.evaluate(x_test, one_hot_test_labels, verbose=0)
-    print(result)
+    print('___________________________')
+    epochs_count(4, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    epochs_count(9, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    epochs_count(32, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    epochs_count(64, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    epochs_count(128, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    print('___________________________')
+    lay_2(1, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    lay_2(2, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    lay_2(3, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    print('___________________________')
+    batch_2(50, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    batch_2(100, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    batch_2(200, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    batch_2(350, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    batch_2(512, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    batch_2(600, partial_x_train, partial_y_train, x_test, one_hot_test_labels, x_val, y_val)
+    print('////////////////////////////////////////////////////')
 
 
 def third_exercise():
-    pass
+    (train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
+    mean = train_data.mean(axis=0)
+    train_data -= mean
+    std = train_data.std(axis=0)
+    train_data /= std
+
+    test_data -= mean
+    test_data /= std
+
+    def build_model():
+        model = models.Sequential()
+        model.add(layers.Dense(64, activation='relu', input_shape=(train_data.shape[1],)))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(1))
+        model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+        return model
+
+    k = 4
+    num_val_samples = len(train_data) // k
+    num_epochs = 100
+    all_scores = []
+
+    for i in range(k):
+        print('Processing fold #', i)
+        val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+        val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+        partial_train_data = np.concatenate(
+            [train_data[:i * num_val_samples],
+             train_data[(i + 1) * num_val_samples:]],
+            axis=0)
+        partial_train_targets = np.concatenate(
+            [train_targets[:i * num_val_samples],
+             train_targets[(i + 1) * num_val_samples:]],
+            axis=0)
+
+        model = build_model()
+        model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=1, verbose=0)
+        val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+        all_scores.append(val_mae)
+        print(all_scores)
+        print(np.mean(all_scores))
 
 
 def main():
-    #first_exercise()
-    second_exercise()
+    # first_exercise()
+    # second_exercise()
     third_exercise()
 
 
